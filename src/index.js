0700d951876b8de10b4b1e82611a0f228bb29e14
MayraMar <ing.mayramartinez@gmail.com>
2022-10-06 15:30:51 -0300

    BrowserRouter basename update

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/harryPotter">
     <App />
   </BrowserRouter>
 );
